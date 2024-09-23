480e48d773b06ef85df5c73c021b9ec598c97e4b
Michael Meinig <michael.meinig@ptvgroup.com>
2024-09-23 09:26:08 +0200

    Add a label, a text box and a "reverse" button to the form.

--- winforms-lib/Form1.Designer.cs
+++ winforms-lib/Form1.Designer.cs
@@ -30,13 +30,16 @@
     {
       buttonOk = new Button();
       buttonCancel = new Button();
+      mLabel = new Label();
+      mTextBox = new TextBox();
+      mButtonReverse = new Button();
       SuspendLayout();
       // 
       // buttonOk
       // 
       buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
       buttonOk.DialogResult = DialogResult.OK;
-      buttonOk.Location = new Point(130, 163);
+      buttonOk.Location = new Point(113, 79);
       buttonOk.Name = "buttonOk";
       buttonOk.Size = new Size(75, 23);
       buttonOk.TabIndex = 0;
@@ -47,12 +50,39 @@
       // 
       buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
       buttonCancel.DialogResult = DialogResult.Cancel;
-      buttonCancel.Location = new Point(225, 163);
+      buttonCancel.Location = new Point(208, 79);
       buttonCancel.Name = "buttonCancel";
       buttonCancel.Size = new Size(75, 23);
       buttonCancel.TabIndex = 1;
       buttonCancel.Text = "Cancel";
       buttonCancel.UseVisualStyleBackColor = true;
+      // 
+      // mLabel
+      // 
+      mLabel.AutoSize = true;
+      mLabel.Location = new Point(16, 22);
+      mLabel.Name = "mLabel";
+      mLabel.Size = new Size(31, 15);
+      mLabel.TabIndex = 2;
+      mLabel.Text = "Text:";
+      // 
+      // mTextBox
+      // 
+      mTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+      mTextBox.Location = new Point(53, 19);
+      mTextBox.Name = "mTextBox";
+      mTextBox.Size = new Size(150, 23);
+      mTextBox.TabIndex = 3;
+      // 
+      // mButtonReverse
+      // 
+      mButtonReverse.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+      mButtonReverse.Location = new Point(209, 19);
+      mButtonReverse.Name = "mButtonReverse";
+      mButtonReverse.Size = new Size(75, 23);
+      mButtonReverse.TabIndex = 4;
+      mButtonReverse.Text = "Reverse";
+      mButtonReverse.UseVisualStyleBackColor = true;
       // 
       // Form1
       // 
@@ -60,17 +90,24 @@
       AutoScaleDimensions = new SizeF(7F, 15F);
       AutoScaleMode = AutoScaleMode.Font;
       CancelButton = buttonCancel;
-      ClientSize = new Size(312, 199);
+      ClientSize = new Size(295, 115);
+      Controls.Add(mButtonReverse);
+      Controls.Add(mTextBox);
+      Controls.Add(mLabel);
       Controls.Add(buttonCancel);
       Controls.Add(buttonOk);
       Name = "Form1";
       Text = "Form1";
       ResumeLayout(false);
+      PerformLayout();
     }
 
     #endregion
 
     private Button buttonOk;
     private Button buttonCancel;
+    private Label mLabel;
+    private TextBox mTextBox;
+    private Button mButtonReverse;
   }
 }
